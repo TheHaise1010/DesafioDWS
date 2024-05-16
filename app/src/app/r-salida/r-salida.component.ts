69c3dba655609e84ca1417957cee31c3a382bd37
TheHaise <jordan.zelayahs@gmail.com>
2024-05-16 16:30:11 -0600

    Salida component works

--- app/src/app/r-salida/r-salida.component.ts
+++ app/src/app/r-salida/r-salida.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { SalidasService } from '../servicios/salidas/salidas.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-r-salida',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './r-salida.component.html',
   styleUrl: './r-salida.component.css'
 })
 export class RSalidaComponent {
 
+  monto:string = ""
+  fecha:Date = new Date()
+  concepto:string = ""
+
+  constructor(private router: Router, private salidasService:SalidasService){}
+
+  postSalidas():void{
+    try {
+      this.salidasService.postSalidas(this.concepto, this.fecha, Number.parseFloat(this.monto)).subscribe(
+        (data) => {
+          console.log(data);
+          Swal.fire(
+            'Excelente',
+            'Entrada agregada exitosamente ',
+            'success'
+          )
+          this.concepto = ""
+          this.monto = "0.00"
+          this.fecha = new Date()
+        }
+      );
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Sucedio un problema al agregar la entrada'
+      })
+      console.log('Ocurrió un error al realizar la solicitud:', error);
+    }
+   }
 }
--- app/src/app/servicios/salidas/salidas.service.ts
+++ app/src/app/servicios/salidas/salidas.service.ts
@@ -9,7 +9,7 @@ export class SalidasService {
   private apiUrl = 'https://663ea49ae1913c4767982df8.mockapi.io/Salidas';
   constructor(private http: HttpClient) { }
 
-  postSalida(concepto:String, fecha:Date, monto:Number):Observable<any>{
+  postSalidas(concepto:String, fecha:Date, monto:Number):Observable<any>{
     return this.http.post<any>(this.apiUrl,{
       concepto:concepto,
       fecha:fecha,
