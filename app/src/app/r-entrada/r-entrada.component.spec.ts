93285f2877cf944dc8c52dbc4f50689c76355e93
TheHaise <jordan.zelayahs@gmail.com>
2024-05-16 17:24:50 -0600

    testing

--- app/src/app/r-entrada/r-entrada.component.spec.ts
+++ app/src/app/r-entrada/r-entrada.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { REntradaComponent } from './r-entrada.component';
+
 describe('REntradaComponent', () => {
   let component: REntradaComponent;
   let fixture: ComponentFixture<REntradaComponent>;
