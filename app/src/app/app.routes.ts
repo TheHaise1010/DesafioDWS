07d9da9139ff250a6a49681dd66b791221ba6ea2
TheHaise <jordan.zelayahs@gmail.com>
2024-05-15 19:17:06 -0600

    first push

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { NavComponent } from './nav/nav.component';
+import { REntradaComponent } from './r-entrada/r-entrada.component';
+import { RSalidaComponent } from './r-salida/r-salida.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
     {path:'registro', component:RegistroComponent},
-    {path:'nav',component:NavComponent}
+    {path:'nav',component:NavComponent},
+    {path:'r-entrada',component:REntradaComponent},
+    {path:'r-salida',component:RSalidaComponent}
 ];
